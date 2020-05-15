215397f03d24e0760bc65e85dcf5616281461fcc
Zicoritonda <zicoritonda@yahoo.co.id>
2020-05-16 00:50:01 +0700

    base main menu

--- client.py
+++ client.py
@@ -1,12 +1,18 @@
 import pygame
 from network import Network
 from player import Player
+import os
 
-width = 500
-height = 500
+# State: "menu", "start", "game"
+state = "menu"
+
+width = 1280
+height = 720
 win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Client")
-
+pygame.display.set_caption("Fligo")
+pygame.font.init()
+font = pygame.font.Font(os.getcwd() + '\\Resources\\SnesItalic-vmAPZ.ttf', 128)
+font2 = pygame.font.Font(os.getcwd() + '\\Resources\\Alice-Regular.ttf', 128)
 
 def redrawWindow(win,player, player2):
     win.fill((255,255,255))
@@ -14,23 +20,86 @@ def redrawWindow(win,player, player2):
     player2.draw(win)
     pygame.display.update()
 
+def main_menu():
+    menu = True
+    # clock = pygame.time.Clock()
 
-def main():
-    run = True
+    while menu:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu = False
+                pygame.quit()
+        
+        # main menu
+        win.fill((255,255,255))
+        mouse = pygame.mouse.get_pos()
+
+        # Button dan Text
+        # Title
+        text = font.render('4. FLIGO', True, (255,0,0)) 
+        textRect = text.get_rect()
+
+        textRect.center = (width // 2, (height // 2) - 100)
+        win.blit(text, textRect)
+
+        # Button
+        pygame.draw.rect(win, (255,0,0),((width // 2)-100,(height // 2),200,50),3)
+        pygame.draw.rect(win, (255,0,0),((width // 2)-100,(height // 2)+75,200,50),3)
+
+        pygame.display.update()
+
+def start_menu():
+    start = True
+    # clock = pygame.time.Clock()
+
+    while start:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu = False
+                pygame.quit()
+        
+        # main menu
+        win.fill((0,255,255))
+        # Button dan Text
+        pygame.display.update()
+
+def game():
+    game = True
     n = Network()
     p = n.getP()
-    clock = pygame.time.Clock()
+    # clock = pygame.time.Clock()
 
-    while run:
+    while game:  
         clock.tick(60)
         p2 = n.send(p)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                game = False
                 pygame.quit()
 
         p.move()
         redrawWindow(win, p, p2)
 
+
+def main():
+    run = True
+
+    while run:  
+        # clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+        if state == "menu":
+            main_menu()
+        if state == "start":
+            start_menu()
+        if state == "game":
+            game()
+
+clock = pygame.time.Clock()
 main()
